228ab9050a180780c6994b9b943d8c6b2d372f9a
Nataliya Pogodina <n.pogodina@gmail.com>
2020-01-19 21:13:31 -0800

    Set up the app, created Blog schema and model, added the Index route.

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express"),
+      app = express(),
+      bodyParser = require("body-parser"),
+      mongoose = require("mongoose"),
+      port = 3000;
+
+// APP CONFIG
+mongoose.connect("mongodb://localhost:27017/blogapp", { useNewUrlParser: true });
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+// MONGOOSE/MODEL CONFIG
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+// RESTFUL ROUTS
+app.get("/", function(req, res){
+    res.redirect("/blogs");
+})
+
+app.get("/blogs", function(req, res){
+    Blog.find({}, function(err, blogs){
+        if(err){
+            console.log("Error");
+        } else {
+            res.render("index", {blogs: blogs});
+        }
+    });
+});
+
+app.listen(port, function(){
+    console.log("Server is running!");
+});
